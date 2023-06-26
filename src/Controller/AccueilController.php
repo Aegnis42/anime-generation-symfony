7ceaa8c02c8153cbbda366d6fdeb108a97785bd5
28010-08-01 <28010-08-01@exchange.ad.afpanet>
2023-06-26 16:30:42 +0200

    first api guzzle and style responce

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,15 +5,65 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use GuzzleHttp\Client;
 
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
+        $client = new Client();
+
+        $response = $client->request('POST', 'https://anilist-graphql.p.rapidapi.com/', [
+            'headers' => [
+                'X-RapidAPI-Host' => 'anilist-graphql.p.rapidapi.com',
+                'X-RapidAPI-Key' => '6c0ccbfc24mshe8f5e23bcd6c445p173993jsne63638fc4bfd',
+                'content-type' => 'application/json',
+            ],
+            'json' => [
+                'query' => '{
+                    Page(page: 1, perPage: 25) {
+                        media(type: ANIME, status: RELEASING, isAdult: false, sort: START_DATE_DESC) {
+                            id
+                            title {
+                                romaji
+                                english
+                                native
+                            }
+                            startDate {
+                                year
+                                month
+                                day
+                            }
+                            format
+                            status
+                            episodes
+                            duration
+                            genres
+                            averageScore
+                            popularity
+                            coverImage {
+                                large
+                            }
+                            bannerImage
+                            description
+                            siteUrl
+                        }
+                    }
+                }'
+            ]
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
             'titre' => 'Accueil',
+            'navNameAccueil' => '',
+            'navNameCatalogue' => 'Catalogue',
+            'navNameAgenda' => 'Agenda',
+            'navNameProfil' => 'Profil',
+            'animes' => $data['data']['Page']['media'],
         ]);
     }
 }
---
+++ src/Controller/CatalogueController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CatalogueController extends AbstractController
+{
+    #[Route('/catalogue', name: 'app_catalogue')]
+    public function index(): Response
+    {
+        return $this->render('catalogue/index.html.twig', [
+            'controller_name' => 'CatalogueController',
+            'titre' => 'Catalogue',
+            'navNameAccueil' => 'Accueil',
+            'navNameCatalogue' => '',
+            'navNameAgenda' => 'Agenda',
+            'navNameProfil' => 'Profil',
+        ]);
+    }
+}
